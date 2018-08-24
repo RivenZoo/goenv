988278a467cc6e27963e3b9e1ffc0a61d437fada
zrw <zrw2010@gmail.com>
2018-08-24 11:48:10 +0800

    [update] add project path variable
    Signed-off-by: zrw <zrw2010@gmail.com>

--- make_gopath.mk
+++ make_gopath.mk
@@ -2,6 +2,7 @@
 # 1. put make_gopath.mk to your project top directory	
 # 2. add `include make_gopath.mk` to your project Makefile
 # 3. make sure target `gopath` to be called
+# 4. `cd $(PROJ_GOPATH)` then build
 
 .PHONY: gopath
 
@@ -11,6 +12,7 @@ OS = $(shell uname)
 GOENV_HOME = $(HOME)/.goenv
 PROJ_DIR = $(dir $(mkfile_path))
 PROJ_NAME = $(shell basename $(PROJ_DIR))
+PROJ_GOPATH =
 GOPATH =
 
 ifeq ($(OS),Linux)
@@ -24,13 +26,15 @@ $(error GOPATH not generate on $(OS))
 endif
 
 GOPATH = ${GOENV_HOME}/$(shell echo $(pathmd5) | cut -c1-16)
+PROJ_GOPATH = $(GOPATH)/src/$(PROJ_NAME)
 
 $(GOPATH): | $(GOPATH)/workdir
 	mkdir -p $(GOPATH) $(GOPATH)/bin $(GOPATH)/src $(GOPATH)/pkg
 	echo $(PROJ_DIR) > $(GOPATH)/workdir
-	ln -s $(PROJ_DIR) $(GOPATH)/src/$(PROJ_NAME)
+	ln -s $(PROJ_DIR) $(PROJ_GOPATH)
 
 gopath: $(GOPATH)
 
 
 export GOPATH
+export PROJ_GOPATH
